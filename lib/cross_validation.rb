39597fb60f9e5040370307f849201c2fdbe5159a
Jon-Michael Deldin <dev@jmdeldin.com>
2013-04-06 21:06:34 -0600

    Require the runner and confusion matrix by default

--- lib/cross_validation.rb
+++ lib/cross_validation.rb
@@ -3,3 +3,7 @@ $LOAD_PATH.unshift File.dirname(__FILE__)
 module CrossValidation
   VERSION = '0.0.1'
 end
+
+%w(confusion_matrix runner).each do |fn|
+  require File.join('cross_validation', fn)
+end
